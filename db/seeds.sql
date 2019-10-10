3700bd1cc20ae1609e881e76a4b611ad4ea35db8
dbell <greenadvisor@gmail.com>
2019-10-10 11:32:42 -0400

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Big Double Burger");
+INSERT INTO burgers (burger_name) VALUES ("Dave's Hot 'n Juicy");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
